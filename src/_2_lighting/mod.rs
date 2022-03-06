07c3be0a957d7dbcae1a8e6c8d29e3e8e9244765
Tommy Lau <tommy.lhg@gmail.com>
2022-03-06 23:40:52 +0800

    2.4.1 lighting maps diffuse map

--- src/_2_lighting/mod.rs
+++ src/_2_lighting/mod.rs
@@ -6,6 +6,7 @@ mod _2_4_basic_lighting_exercise2;
 mod _2_5_basic_lighting_exercise3;
 mod _3_1_materials;
 mod _3_2_materials_exercise1;
+mod _4_1_lighting_maps_diffuse_map;
 
 pub use _1_colors::*;
 pub use _2_1_basic_lighting_diffuse::*;
@@ -15,3 +16,4 @@ pub use _2_4_basic_lighting_exercise2::*;
 pub use _2_5_basic_lighting_exercise3::*;
 pub use _3_1_materials::*;
 pub use _3_2_materials_exercise1::*;
+pub use _4_1_lighting_maps_diffuse_map::*;
