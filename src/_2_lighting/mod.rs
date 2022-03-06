46d7b7a5c709d6ab9044ec61b49786bd665b8671
Tommy Lau <tommy.lhg@gmail.com>
2022-03-06 23:57:08 +0800

    2.4.2 lighting maps specular map

--- src/_2_lighting/mod.rs
+++ src/_2_lighting/mod.rs
@@ -7,6 +7,7 @@ mod _2_5_basic_lighting_exercise3;
 mod _3_1_materials;
 mod _3_2_materials_exercise1;
 mod _4_1_lighting_maps_diffuse_map;
+mod _4_2_lighting_maps_specular_map;
 
 pub use _1_colors::*;
 pub use _2_1_basic_lighting_diffuse::*;
@@ -17,3 +18,4 @@ pub use _2_5_basic_lighting_exercise3::*;
 pub use _3_1_materials::*;
 pub use _3_2_materials_exercise1::*;
 pub use _4_1_lighting_maps_diffuse_map::*;
+pub use _4_2_lighting_maps_specular_map::*;
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod _2_lighting;
 use _1_getting_started::*;
 use _2_lighting::*;
 
-const MAIN_PROGRAM: &str = "2.4.1";
+const MAIN_PROGRAM: &str = "2.4.2";
 
 fn main() {
     let mut main = MAIN_PROGRAM;
@@ -61,6 +61,7 @@ fn main() {
         "2.3.1" => main_2_3_1(),
         "2.3.2" => main_2_3_2(),
         "2.4.1" => main_2_4_1(),
+        "2.4.2" => main_2_4_2(),
 
         _ => {}
     }
